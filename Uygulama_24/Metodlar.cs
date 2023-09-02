7676461729cc5c06804ca5b4d935a5cd6c95d062
amged <amged@DESKTOP-9MT6K87>
2023-09-02 17:17:00 +0300

    Add project files.

---
+++ Uygulama_24/Metodlar.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uygulama_24
+{
+    internal class Metodlar
+    {
+        //***************************** Soru 1 Metodlar ****************************************//
+        public string ad, soyad, sehir;
+        public int yas;
+
+        public void Yazdir()
+        {
+            Console.WriteLine("{0} {1} - {2} - {3} yaş", ad, soyad, sehir, yas);
+        }
+
+
+
+        //***************************** Soru 2 Metodlar ****************************************//
+
+        //public int ToplamaIslemi(int[] sayilar)
+        //    {
+        //        int toplam = 0;
+        //        foreach (int i in sayilar)
+        //        {
+        //            toplam += i;
+
+        //        }
+        //        return toplam;
+        //    }
+        //    public void ToplamiveOrtalamayiYazdir(int tplm, int adet)
+        //    {
+        //        Console.WriteLine("Sayıların Toplamı = " + tplm);
+        //        Console.WriteLine("Sayıların Ortalaması = " + (double)tplm / adet);
+        //    }
+
+        //    //***************************** Soru 3 Metodlar ****************************************//
+
+        //    public string StringIste(string baslik)
+        //    {
+        //        string text = string.Empty;
+        //        bool hata = true;
+        //        do
+        //        {
+        //            Console.Write(baslik);
+        //            text = Console.ReadLine();
+        //            if (text.Length == 0)
+        //            {
+        //                Console.WriteLine("String değeri boş bırakılamaz");
+        //                hata = true;
+        //            }
+        //            else
+        //            {
+        //                hata = false;
+        //            }
+
+        //        } while (hata);
+        //        return text;
+
+        //    }
+
+        //    //***************************** Soru 4 Metodlar ****************************************//
+
+        //    public int Topla(int[] kullanicininSayilari)
+        //    {
+        //        int toplam = 0;
+        //        foreach (int i in kullanicininSayilari)
+        //        {
+        //            toplam += i;
+        //        }
+        //        return toplam;
+        //    }
+
+        //    public double Ortalama(int toplam, int adet)
+        //    {
+        //        double ort = (double)toplam / adet;
+        //        return ort;
+
+        //    }
+
+        //    public void EnKucuk(int[] sayilar)
+        //    {
+        //        Array.Sort(sayilar);
+        //        Console.WriteLine("Sayıların En Küçüğü : " + sayilar[0]);
+        //    }
+        //    public void EnBuyuk(int[] sayilar)
+        //    {
+        //        Array.Sort(sayilar);
+        //        Array.Reverse(sayilar);
+        //        Console.WriteLine("Sayıların En Büyüğü : " + sayilar[0]);
+        //    }
+
+        //    //***************************** Soru 5 Metodlar ****************************************//
+
+        //    public void Hesapla(int s1, int s2)
+        //    {
+        //        Console.WriteLine("Geçerli Bir İşlem Türü Girmediğiniz İçin Sayılar Toplanacak");
+        //        Console.WriteLine("Sayıların Toplamı = " + (s1 + s2));
+        //    }
+        //    public void Hesapla(int s1, int s2, string islemTuru)
+        //    {
+        //        string format = "{0} {1} {2} = {3}";
+        //        double sonuc = 0;
+        //        switch (islemTuru)
+        //        {
+        //            case "+":
+        //                sonuc = s1 + s2;
+        //                break;
+        //            case "-":
+        //                sonuc = s1 - s2;
+        //                break;
+        //            case "*":
+        //                sonuc = s1 * s2;
+        //                break;
+        //            case "/":
+        //                sonuc = (double)s1 / s2;
+        //                break;
+
+        //        }
+        //        Console.WriteLine(format, s1, islemTuru, s2, sonuc);
+        //    }
+
+        //    //***************************** Soru 6 Metodlar ****************************************//
+
+        //    public void Topla(int s1, int s2)
+        //    {
+        //        Console.WriteLine("1.Metod");
+        //        Console.WriteLine(s1 + s2);
+        //        Console.WriteLine("--------------------");
+        //    }
+        //    public void Topla(int s1, int s2, int s3)
+        //    {
+        //        Console.WriteLine("2.Metod");
+        //        Console.WriteLine(s1 + s2 + s3);
+        //        Console.WriteLine("----------------------");
+        //    }
+        //    public void Topla(double s1, double s2)
+        //    {
+        //        Console.WriteLine("3.Metod");
+        //        Console.WriteLine(s1 + s2);
+        //        Console.WriteLine("----------------------");
+        //    }
+        //    public void Topla(double s1, double s2, double s3)
+        //    {
+        //        Console.WriteLine("4.Metod");
+        //        Console.WriteLine(s1 + s2 + s3);
+        //        Console.WriteLine("----------------------");
+        //    }
+        //    public void Topla()
+        //    {
+        //        Console.WriteLine("Parametre Girmediğiniz İçin Herhangi bir Sayı Toplanmadı");
+        //    }
+
+    }
+}
---
+++ Uygulama_24/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Uygulama_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Metodlar> kisiler = new List<Metodlar>()
+            {
+                new Metodlar() {ad = "Ahmet", soyad = "Kaçar", sehir = "İzmir", yas = 30 },
+                new Metodlar() {ad = "Ayşe", soyad = "Sarı", sehir = "İzmir", yas = 25 },
+                new Metodlar() {ad = "Mehmet", soyad = "Yılmaz", sehir = "Ankara", yas = 35 },
+                new Metodlar() {ad = "Halil", soyad = "Sönmez", sehir = "Manisa", yas = 28 },
+                new Metodlar() {ad = "Fatma", soyad = "Sönmez", sehir = "Tokat", yas = 45 },
+                new Metodlar() {ad = "Elif", soyad = "Taşçı", sehir = "Bilecik", yas = 22 },
+                new Metodlar() {ad = "Elif", soyad = "Bilgin", sehir = "İstanbul", yas = 29 },
+                new Metodlar() {ad = "Elif", soyad = "Çetiner", sehir = "İstanbul", yas = 30 },
+                new Metodlar() {ad = "Ahmet", soyad = "Kaçar", sehir = "İstanbul", yas = 33 },
+                new Metodlar() {ad = "Hakan", soyad = "Uysal", sehir = "Ankara", yas = 40 },
+                new Metodlar() {ad = "Leyla", soyad = "Kabakçı", sehir = "Zonguldak", yas = 27 },
+                new Metodlar() {ad = "Ali", soyad = "Yılmaz", sehir = "İstanbul", yas = 28  },
+                new Metodlar() {ad = "Selma", soyad = "Demir", sehir = "Ankara", yas = 32 }
+            };
+
+            #region Orderby(asc)
+
+            Console.WriteLine("*******************************************************");
+            Console.WriteLine("OrderBy Metod\r\nVerileri küçükten büyüğe doğru sıralamak Classic olarak");
+            Console.WriteLine("*******************************************************");
+            Console.WriteLine();
+            var kbListe = from k in kisiler
+                          orderby k.ad ascending
+                          select k;
+            foreach (var item in kbListe)
+            {
+                item.Yazdir();
+            }
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("*******************************************************");
+            Console.WriteLine("OrderBy Metod\r\nVerileri küçükten büyüğe doğru sıralamak");
+            Console.WriteLine("*******************************************************");
+            Console.WriteLine();
+
+            var kbListe2 = kisiler.OrderBy(k => k.ad);
+            foreach (var item in kbListe2)
+            {
+                item.Yazdir();
+            }
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine();
+
+            #endregion
+
+
+
+            Console.WriteLine("*******************************************************");
+            Console.WriteLine("OrderByDescending Metod\r\nverileri ad a göre alfabetik, soyad a göre alfabetik ters sıralama Classic olarak");
+            Console.WriteLine("*******************************************************");
+            Console.WriteLine();
+
+            Console.WriteLine();
+            #region OrderByDescending
+            var bkListe = from x in kisiler
+                          orderby x.ad descending
+                          select x;
+            foreach (var item in bkListe)
+            {
+                item.Yazdir();
+            }
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("*******************************************************");
+            Console.WriteLine("OrderByDescending Metod\r\nverileri ad a göre alfabetik, soyad a göre alfabetik ters sıralama Yapar");
+            Console.WriteLine("*******************************************************");
+            Console.WriteLine();
+
+
+            var bkListe2 = kisiler.OrderByDescending(x => x.ad);
+            foreach (var item in bkListe2)
+            {
+                item.Yazdir();
+            }
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine();
+            #endregion
+
+
+
+            #region ThenBy veya ThenByDescending
+            Console.WriteLine("*******************************************************");
+            Console.WriteLine("ThenByDescending Metod\r\nVerileri ad a göre alfabetik, soyad a göre alfabetik ters sıralamk Classic olarak");
+            Console.WriteLine("*******************************************************");
+            Console.WriteLine();
+
+            var kbAListe = from o in kisiler
+                           orderby o.ad ascending, o.soyad descending
+                           select o;
+            foreach (var item in kbAListe)
+            {
+                item.Yazdir();
+            }
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine();
+
+
+            Console.WriteLine("*******************************************************");
+            Console.WriteLine("ThenByDescending Metod\r\nVerileri ad a göre alfabetik, soyad a göre alfabetik ters sıralamk");
+            Console.WriteLine("*******************************************************");
+            Console.WriteLine();
+
+            var kbAListe2 = kisiler.OrderBy(o => o.ad).ThenByDescending(o => o.soyad);
+            foreach (var item in kbAListe2)
+            {
+                item.Yazdir();
+            }
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine();
+            #endregion
+
+            #region Where
+            Console.WriteLine("*******************************************************");
+            Console.WriteLine("where Metod\r\nKoleksiyon içerisinde şehri İstanbul olan kişileri listeleyelim Classic olarak");
+            Console.WriteLine("*******************************************************");
+            Console.WriteLine();
+
+            var fListe = from k in kisiler
+                         where k.sehir.ToLower() == "istanbul"
+                         select k;
+            foreach (var item in fListe)
+            {
+                item.Yazdir();
+            }
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine();
+
+
+            Console.WriteLine("*******************************************************");
+            Console.WriteLine("where Metod\r\nKoleksiyon içerisinde şehri İstanbul olan kişileri listeleyelim Classic olarak");
+            Console.WriteLine("*******************************************************");
+            Console.WriteLine();
+
+            var fListe2 = kisiler.Where(k => k.sehir.ToLower() == "istanbul");
+            foreach (var item in fListe2)
+            {
+                item.Yazdir();
+            }
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine();
+            #endregion
+
+
+            #region GroupBy
+
+            Console.WriteLine("*******************************************************");
+            Console.WriteLine("GroupBy Metod\r\nKoleksiyon içerisindeki verileri şehir bilgisine göre gruplayalım Classic olarak");
+            Console.WriteLine("*******************************************************");
+            Console.WriteLine();
+
+            var gListe = kisiler.GroupBy(g => g.sehir);
+            foreach (var grup in gListe)
+            {
+                Console.WriteLine(grup.Key);
+                Console.WriteLine("*******************************************************");
+                foreach (var item in grup.OrderBy(k => k.ad))
+                {
+                    item.Yazdir();
+                }
+                Console.WriteLine("*******************************************************");
+                Console.WriteLine();
+            }
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
